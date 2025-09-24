612a1da71958af6e6fac6794ab78c5605df82719
nahue-v <nahuelvera3770@gmail.com>
2025-09-24 14:40:53 -0300

    feat: add functionality for Order Controllers

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Transport Management API')
+    .setDescription('Domentation for Transport Management API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,12 +1,80 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { Role } from '@prisma/client';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderStatusDto } from './dto/update-order-status.dto';
 
 @Controller('orders')
+@UseGuards(AuthGuard('jwt'), RolesGuard)
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
+  @Post()
+  @Roles(Role.CLIENT)
+  create(@Body() dto: CreateOrderDto, @Req() req: any) {
+    return this.ordersService.create(dto, req.user);
+  }
+
   @Get()
+
+  /**
+   * Retrieve all orders
+   * @returns All orders
+   */
   findAll() {
     return this.ordersService.findAll();
+  }
+
+  @Get(':id')
+  /**
+   * Find an order by its id
+   * @param id The id of the order to find
+   * @returns The found order
+   */
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.ordersService.findOne(+id);
+  }
+
+  @Patch(':id')
+  @Roles(Role.ADMIN, Role.OPERATOR)
+  /**
+   * Update the state of an order
+   * @param id The id of the order to update
+   * @param dto The UpdateOrderStatusDto containing the new state
+   * @param req The request object containing the user making the request
+   * @returns The updated order
+   */
+  updateEstado(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateOrderStatusDto,
+    @Req() req: any,
+  ) {
+    return this.ordersService.updateState(id, dto, req.user);
+  }
+
+  @Delete(':id')
+  @Roles(Role.ADMIN)
+  /**
+   * Delete an order by its id
+   * @param id The id of the order to delete
+   * @param req The request object containing the user making the request
+   * @returns The deleted order
+   */
+  remove(@Param('id', ParseIntPipe) id: number, @Req() req: any) {
+    return this.ordersService.remove(id, req.user);
   }
 }
